3baee12fb447e66f547fcc334c93d088a0e39862
zuohuaiyu <huaiyu.zuo@foxmail.com>
2020-09-06 17:34:29 +0800

    <feat>: update quick sort implement

--- Algorithm/Sort/QuickSort.js
+++ Algorithm/Sort/QuickSort.js
@@ -1,3 +1,20 @@
-function quickSort(array, start, end) {
-    
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let index = Math.random() * arr.length;
+  let pivot = arr.splice(index, 1)[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
 }
+
+let arr = [2, 3, 1, 4, 5];
+console.log(quickSort(arr));
